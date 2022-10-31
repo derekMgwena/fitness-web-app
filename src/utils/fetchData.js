0f52b6c75e4f76ea19069ff76f47e7ead95bbd9f
derekMgwena <derekmakhubela@gmail.com>
2022-10-31 10:25:46 +0200

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,35 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'f8bad06311msh398899cea0b798bp1b4e5cjsnb5143edddc13',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'f8bad06311msh398899cea0b798bp1b4e5cjsnb5143edddc13',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
+/* const options = {
+  method: 'GET',
+  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+  headers: {
+    'X-RapidAPI-Key': 'f8bad06311msh398899cea0b798bp1b4e5cjsnb5143edddc13',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};*/
+
+/* headers: {
+    'X-RapidAPI-Key': 'f8bad06311msh398899cea0b798bp1b4e5cjsnb5143edddc13',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }*/
